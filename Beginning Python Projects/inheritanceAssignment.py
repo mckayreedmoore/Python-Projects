5b8afcedaa679b00e8c0b3a8139c518b01c7d3a2
mckayreedmoore <75471605+mckayreedmoore@users.noreply.github.com>
2022-02-11 13:53:47 -0700

    Updated Homework

---
+++ Beginning Python Projects/inheritanceAssignment.py
@@ -0,0 +1,17 @@
+#creation of class driver
+class driver:
+    car = ""
+    activeLicense = ""
+
+#creation of child class of driver, taxi
+class taxi(driver):
+    taxiLicense = ""
+    base_pay = ""
+
+#creation of child class of driver, bus
+class bus(driver):
+    routes = ""
+    activeCDL = ""
+    base_pay = ""
+
+
