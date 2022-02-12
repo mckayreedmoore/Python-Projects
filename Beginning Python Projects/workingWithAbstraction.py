07db51aa6b7321312ed2824308e030c1bc86d671
mckayreedmoore <75471605+mckayreedmoore@users.noreply.github.com>
2022-02-12 13:08:27 -0700

    Updated Homework

--- Beginning Python Projects/workingWithAbstraction.py
+++ Beginning Python Projects/workingWithAbstraction.py
@@ -1,14 +1,19 @@
-
+#creation of a parent class
 class polygon:
-    def sides(shelf):
+    #creation of an abstract method
+    def sides(self):
         print('I am a polygon! I have 3 or more sides')
+    #creation of a normal method
     def shapeCelebrate(self):
         print('Shapes sure are cool!')
-
+#creation of a child class
 class quadrilateral(polygon):
+    #modification of the parent class method to fit
+        #child class
     def sides(self):
         print('I am a quadrilateral! I have 4 sides')
 
+#instatiation of all classes and methods
 shape = polygon()
 shape4 = quadrilateral()
 
