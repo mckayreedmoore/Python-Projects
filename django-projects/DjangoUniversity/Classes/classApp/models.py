f7419fa56324f5a95d27ff4e0e18aa7f3cf9b591
mckayreedmoore <75471605+mckayreedmoore@users.noreply.github.com>
2022-02-18 12:51:01 -0700

    Updated Homework

--- django-projects/DjangoUniversity/Classes/classApp/models.py
+++ django-projects/DjangoUniversity/Classes/classApp/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class djangoClasses(models.Model):
-    title = models.CharField(max_length=60),
-    courseNum = models.IntegerField(max_length=10),
-    instructorName = models.CharField(max_length=60),
-    duration = models.FloatField(max_length=10),
+    title = models.CharField(max_length=60)
+    courseNum = models.IntegerField()
+    instructorName = models.CharField(max_length=60)
+    duration = models.FloatField()
 
     def __str__(self):
         return self.title
