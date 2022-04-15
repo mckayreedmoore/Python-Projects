1760b12de7ae280ea74c116ff23776cedd0f985a
mckayreedmoore <75471605+mckayreedmoore@users.noreply.github.com>
2022-04-15 17:30:13 -0600

    Updated Homework

--- pythonProject/django-projects/DjangoUniversity/Classes/classApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class djangoClasses (models.Model):
-    title = models.CharField(max_length=60),
-    courseNumber = models.IntegerField(max_length=8),
-    instructorName = models.CharField(max_length=60),
-    duration = models.FloatField(max_length=10),
